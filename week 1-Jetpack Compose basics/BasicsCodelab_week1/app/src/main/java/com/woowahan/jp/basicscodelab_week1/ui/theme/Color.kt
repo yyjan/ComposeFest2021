b405ed951f6c1d7e6ecd62a3cf30d91741a4348a
yun <yyjan219@gmail.com>
2021-12-10 00:31:20 +0900

    Update week 1 codelab - Basic

--- week 1-Jetpack Compose basics/BasicsCodelab_week1/app/src/main/java/com/woowahan/jp/basicscodelab_week1/ui/theme/Color.kt
+++
@@ -1,8 +0,0 @@
-package com.woowahan.jp.basicscodelab_week1.ui.theme
-
-import androidx.compose.ui.graphics.Color
-
-val Navy = Color(0xFF073042)
-val Blue = Color(0xFF4285F4)
-val LightBlue = Color(0xFFD7EFFE)
-val Chartreuse = Color(0xFFEFF7CF)
